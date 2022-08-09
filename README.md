51a5009a033552e68ba9c26adc27c9835ae11104
0xKubitus <n.cubilier@gmail.com>
2022-08-10 01:34:20 +0200

    start project

---
+++ README.md
@@ -0,0 +1,12 @@
+TEMPLATE USING CREATE REACT APP + ESLINT + PRETTIER.
+
+---
+
+If you're going for a Single Page Application, rename 'Home.jsx'
+into 'App.jsx'. Move it to 'src' folder and delete the
+'pages' folder.
+(don't forget to modify your imports accordingly)
+
+---
+
+Run 'npm start' to launch the Application.
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+// import
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>TEST</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
