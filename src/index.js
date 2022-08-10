1ddbd8b28358a776795f116f79b5d6b2da32ff57
0xKubitus <n.cubilier@gmail.com>
2022-08-10 15:58:26 +0200

    removing useless 'App.jsx' file (transfered into 'src/index.js' + useState for theme mode managed by function ThemeProvider in ThemeContext.jsx => needs to be connected to the buttons in header

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-import App from 'components/App';
+import Header from 'components/Header';
+import Home from 'pages/Home';
+import About from 'pages/About';
+import Works from 'pages/Works';
+
+import { ThemeProvider } from 'context/ThemeContext';
 
 import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <ThemeProvider>
+                <Header />
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/works" element={<Works />} />
+                    </Routes>
+                </div>
+            </ThemeProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
